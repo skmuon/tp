5630c635683f82d9f658aff9e24863e0f7de263b
Karthi <skn.muon@gmail.com>
2017-02-03 20:39:15 +0530

    Exercise 2.4-3 from TP

---
+++ chapter_2/ex2.4.3.py
@@ -0,0 +1,8 @@
+"""
+If I leave my house at 6.52 am and I run 1 mile at an easy pace (8:15 per mile),
+then 3 miles at temp (7:12 per mile) and 1 mile at easy pace again, what time
+do I get home for breakfast?
+"""
+
+
+# Converting everything to minutes (The question was confusing, especially the notation)
