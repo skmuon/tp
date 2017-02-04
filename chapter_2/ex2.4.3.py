ce295187047294983a153bab8a6f7a5122a90e7a
Karthi <skn.muon@gmail.com>
2017-02-04 16:31:17 +0530

    Added solution

--- chapter_2/ex2.4.3.py
+++ chapter_2/ex2.4.3.py
@@ -6,3 +6,13 @@ do I get home for breakfast?
 
 
 # Converting everything to minutes (The question was confusing, especially the notation)
+
+starting_hour = 6
+starting_mins = 52
+
+total_time_for_the_run_in_mins = (((7*60) + 12)*2 + ((8*60)+15)*3)/60
+
+ending_mins = (starting_mins + total_time_for_the_run_in_mins) % 60
+ending_hour = starting_hour + 1
+print "Arrival time is %d:%d" % (ending_hour , ending_mins)
+
